cad596ba16bd7a8445ad48fb95407c7f155853fd
JosephDelaneJanicki <47838316+JosephDelaneJanicki@users.noreply.github.com>
2024-09-03 02:16:32 -0500

    Completed R scenario and clarified instructions to allow for flexibility.

--- sql-query-from-database.sql
+++ sql-query-from-database.sql
@@ -1,3 +1,13 @@
-SELECT unit_id, SUM(applicants), fiscal_year, fiscal_month
-FROM funnel
-GROUP BY unit_id;
+-- the select statement was incorrect and was missing the "Sum of applicants" field the R script expects. 
+-- for readability I also indented the script
+SELECT 
+    unit_id, 
+    SUM(applicants) AS sum_of_applicants, 
+    fiscal_year, fiscal_month
+FROM 
+    funnel
+GROUP BY 
+    unit_id; 
+    -- added missing fields
+    fiscal_year, fiscal_month;
+    
